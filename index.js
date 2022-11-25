c0ee0bc16690b284743f7034e5489b1898b21df4
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-25 14:05:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function receivesAFunction(callback) {
+     callback();
+}
+
+function returnsANamedFunction(){
+    const named= function(){}
+    return named
+}
+function returnsAnAnonymousFunction(){
+    return (function ( ){} )
+
+}
